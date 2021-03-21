a7d0e05a2b568c58a6c4cd2f8a9ebe56da372421
JL-OLEMAR <joseluis19963@gmail.com>
2021-03-20 21:21:25 -0500

    conecar react con socket

---
+++ models/sockets.js
@@ -0,0 +1,26 @@
+const Marcadores = require('./marcadores')
+
+class Sockets {
+  constructor (io) {
+    this.io = io
+
+    this.marcadores = new Marcadores()
+
+    this.socketEvents()
+  }
+
+  socketEvents () {
+    // On connection
+    this.io.on('connection', (socket) => {
+      console.log('Cliente connectado')
+
+      // TODO: marcadores-activos
+
+      // TODO: marcador-nuevo
+
+      // TODO: marcador-actualizado
+    })
+  }
+}
+
+module.exports = Sockets
